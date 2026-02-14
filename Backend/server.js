d14bd17426f76eb625f143479b54c0f80b98e9e8
youssef <youssef.magdy.office@gmail.com>
2026-02-14 14:54:31 +0200

    update deploment info

--- Backend/server.js
+++ Backend/server.js
@@ -16,6 +16,8 @@ const path = require('path');
 // Middleware
 app.use(cors({
     origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
 
