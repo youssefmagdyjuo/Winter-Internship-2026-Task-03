ad91863f07c14b6b9a9cc00661f895586a9a50dc
youssef <youssef.magdy.office@gmail.com>
2026-02-14 16:30:20 +0200

    update deploment info

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("mvec_token");
     return token ? <Outlet /> : <Navigate to="/login-signup" />;
 };
 
